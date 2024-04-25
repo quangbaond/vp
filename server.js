8aba36d793c7cbef9cdcade69606464c5e9b15f4
quangbaond <quangbaorp@gmail.com>
2024-04-26 06:20:31 +0700

    develop

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.set('views', './views');
 app.use(cors({
     origin: '*'
 }));
-app.use(siofu.router).listen(3004);
+app.use(siofu.router).listen(3005);
 
 io.on("connection", function (socket) {
     var uploader = new siofu();
